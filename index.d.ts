a16911fd130db54d149aa1f6ec11c87f704372e1
qwertypnk <qwertypnk@protonmail.com>
2021-10-10 12:41:20 +0300

    refactor: update

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,15 @@
 declare class StorageDriver {
   public storageUrl: null | string;
-  public clientSecret: null | string;
+  public clientName: null | string;
   public clientId: null | string;
+  public clientSecret: null | string;
   public clientKey: null | string;
 
   constructor(
     storageUrl: string,
-    clientSecret: null | string,
+    clientName: string,
     clientId: string,
+    clientSecret: null | string,
     clientKey: string,
     preferSecure: boolean,
   );
@@ -15,6 +17,12 @@ declare class StorageDriver {
   // alias for assetLink(filepath)
   alink(filepath: undefined | null | string): null | string;
   assetLink(filepath: undefined | null | string): null | string;
+
+  storeAsset(filepath: undefined | null | string, asset: string | NodeJS.ArrayBufferView): Promise<any>;
+
+  removeAsset(filepath: undefined | null | string): Promise<any>;
+
+  checkAssetUrl(url: undefined | null | string): Promise<any>;
 }
 
 export { StorageDriver };
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const FormData = require('form-data');
+const { fetch } = require('cross-fetch');
+
 class StorageDriver {
-  constructor(storageUrl, clientSecret, clientId, clientKey, preferSecure) {
+  constructor(storageUrl, clientName, clientId, clientSecret, clientKey, preferSecure) {
     this.storageUrl = null;
-    this.clientSecret = null;
+    this.clientName = null;
     this.clientId = null;
+    this.clientSecret = null;
     this.clientKey = null;
 
     if (typeof storageUrl === 'string') {
@@ -14,8 +18,9 @@ class StorageDriver {
       }
     }
     this.storageUrl = storageUrl;
-    this.clientSecret = clientSecret || null;
+    this.clientName = clientName || null;
     this.clientId = clientId || null;
+    this.clientSecret = clientSecret || null;
     this.clientKey = clientKey || null;
   }
 
@@ -31,6 +36,87 @@ class StorageDriver {
       filepath = filepath.substr(1, filepath.length);
     }
     return `${this.storageUrl}/asset/${this.clientKey}/${filepath}`;
+  }
+
+  storeAsset(filepath, asset) {
+    return new Promise((resolve, reject) => {
+      const form = new FormData();
+      form.append('clientName', this.clientName);
+      form.append('clientId', this.clientId);
+      form.append('clientSecret', this.clientSecret);
+      form.append('filepath', filepath);
+      form.append('asset', asset);
+
+      form.submit(`${this.storageUrl}/asset/store`, (err, res) => {
+        try {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          const chunks = [];
+          res.on('data', (chunk) => {
+            chunks.push(chunk);
+          });
+          res.on('end', () => {
+            const result = JSON.parse(Buffer.concat(chunks).toString());
+            result.success ? resolve(result) : reject(result);
+          });
+
+          res.resume();
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  async removeAsset(filepath) {
+    return await new Promise((resolve, reject) => {
+      const form = new FormData();
+      form.append('clientName', this.clientName);
+      form.append('clientId', this.clientId);
+      form.append('clientSecret', this.clientSecret);
+      form.append('clientKey', this.clientKey);
+      form.append('filepath', filepath);
+
+      form.submit(`${this.storageUrl}/asset/remove`, (err, res) => {
+        try {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          const chunks = [];
+          res.on('data', (chunk) => {
+            chunks.push(chunk);
+          });
+          res.on('end', () => {
+            const result = JSON.parse(Buffer.concat(chunks).toString());
+            result.success ? resolve(result) : reject(result);
+          });
+
+          res.resume();
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  async checkAssetUrl(url) {
+    const formData = new FormData();
+    formData.append('clientName', this.clientName);
+    formData.append('clientId', this.clientId);
+    formData.append('clientSecret', this.clientSecret);
+    formData.append('clientKey', this.clientKey);
+    formData.append('url', url);
+    return await (
+      await fetch(`${this.storageUrl}/asset/check`, {
+        method: 'POST',
+        body: formData,
+      })
+    ).json();
   }
 }
 
